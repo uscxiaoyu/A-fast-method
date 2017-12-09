f6cf59fc35c3d9f0574736cf01ec39ea5ec7f5f2
uscxiaoyu <yx_402@usc.edu>
2017-12-09 21:28:11 +0800

    更改
    生成扩散数据集，并用bass模型估计

--- estimate_abm_data.py
+++ estimate_abm_data.py
@@ -2,21 +2,47 @@
 import estimate_bass as eb
 import numpy as np
 import time
+import os
+import multiprocessing
+
+path = 'data/'
+file_list = []
 
 
-data = np.load('gnm_random_graph(10000,30000).npy')
-params_cont = []
-for i, x in enumerate(data[:20]):
+def vst_dir(path, exclude='estimate', include='.npy'):
+    for x in os.listdir(path):
+        sub_path = os.path.join(path, x)
+        if os.path.isdir(sub_path):
+            vst_dir(sub_path)
+        else:
+            if include in sub_path.lower() and exclude not in sub_path.lower():
+                file_list.append(sub_path)
+
+def func(x, para_range):
     p, q = x[:2]
     s_full = x[2:]
     max_ix = np.argmax(s_full)
     s = s_full[:max_ix + 2]
-    t1 = time.clock()
-    para_range = [[1e-6, 0.1], [1e-4, 1], [0, 50000]]
     bassest = eb.Bass_Estimate(s, para_range)
     params = bassest.optima_search(c_n=100, threshold=10e-8)
-    print(i, 'p:%.4f   q:%.4f   m:%d   r2:%.4f' % tuple(params), end=' ')
-    print('Time elapsed: %.2fs' % (time.clock() - t1))
-    params_cont.append([p, q] + list(params))
+    return [p, q] + list(params)
 
-#np.save('estimate gnm_random_graph(10000,30000)_new', params_cont)
+if __name__ == '__main__':
+    vst_dir(path)
+    for txt in file_list:
+        diff_data = np.load(txt)
+        pool = multiprocessing.Pool(processes=6)
+        para_range = [[1e-6, 0.1], [1e-4, 1], [0, 50000]]
+        result = []
+        t1 = time.clock()
+        for x in diff_data:
+            result.append(pool.apply_async(func, (x, para_range)))
+
+        pool.close()
+        pool.join()
+        to_save = []
+        for res in result:
+            to_save.append(res.get())
+
+        print txt, ': Time elapsed: %.2fs' % (time.clock() - t1)
+        np.save(txt[:5] + 'estimate ' + txt[5:-4], to_save)
--- generate_abm_data.py
+++ generate_abm_data.py
@@ -1,8 +1,10 @@
 #coding=utf-8
+from __future__ import division
 import numpy as np
 import networkx as nx
 import time
 import random
+import multiprocessing
 
 
 class Diffuse:  # 默认网络结构为节点数量为10000，边为30000的随机网络
@@ -38,24 +40,70 @@ class Diffuse:  # 默认网络结构为节点数量为10000，边为30000的随�
         return [self.single_diffuse() for i in range(repetes)]
 
 
-class Diffuse_gmm(Diffuse):
+class Diffuse_gmm(Diffuse):  # social influence
+    def __init__(self, p, q, alpha, g=nx.gnm_random_graph(10000, 30000), num_runs=30):
+        if not nx.is_directed(g):
+            self.g = g.to_directed()
+        self.p, self.q = p, q
+        self.alpha = alpha
+        self.num_runs = num_runs
+
     def decision(self, i):  # gmm决策规则
-        dose = sum([self.g.node[k]['state'] for k in self.predecessors(i)])
-        prob = 1 - (1 - self.p) * (1 - self.q) ** dose
+        dose = sum([self.g.node[k]['state'] for k in self.g.predecessors(i)])
+        prob = 1 - (1 - self.p) * (1 - self.q) ** (dose / self.g.in_degree(i) ** self.alpha) if self.g.in_degree(i) else self.p
         return True if random.random() <= prob else False
 
 
-if __name__ == '__main__':
-    pq_range = [(i, j) for i in np.linspace(0.001, 0.021, num=5) for j in np.linspace(0.05, 0.145, num=20)]
-    result = []
-    u = 1
-    g = nx.gnm_random_graph(10000, 30000)
-    for p, q in pq_range:
-        t1 = time.clock()
-        diff = Diffuse_gmm(p, q, g=g, num_runs=30)
-        x = np.mean(diff.repete_diffuse(), axis=0)
-        result.append(np.concatenate(([p, q], x)))
-        print u, 'Time: %.2f s' % (time.clock() - t1)
-        u += 1
+def generate_random_graph(degre_sequance):
+    G = nx.configuration_model(degre_sequance, create_using=None, seed=None)
+    G = nx.Graph(G)
+    G.remove_edges_from(G.selfloop_edges())
 
-    np.save('gnm_random_graph(10000, 30000)-gmm', result)
+    num_of_edges = nx.number_of_edges(G)
+    edges_list = G.edges()
+
+    if num_of_edges > 30000:
+        edges_to_drop = num_of_edges - 30000
+        x = np.random.choice(num_of_edges, edges_to_drop, replace=False)
+        for i in x:
+            a, b = edges_list[i]
+            G.remove_edge(a, b)
+    elif num_of_edges < 30000:
+        edges_to_add = 30000 - num_of_edges
+        x = np.random.choice(10000, edges_to_add * 2, replace=False)
+        to_add_list = zip(x[:edges_to_add], x[edges_to_add:])
+        G.add_edges_from(to_add_list)
+    else:
+        pass
+
+    return G
+
+def func(p, q, alpha, g):
+    diff = Diffuse_gmm(p, q, alpha, g, num_runs=30)
+    x = np.mean(diff.repete_diffuse(), axis=0)
+    return np.concatenate(([p, q], x))
+
+
+if __name__ == '__main__':
+    path = 'C:/Users/XIAOYU/Desktop/data/4 social influence rule/'
+    txt_cont = ['gnm_random_graph(10000,30000),0.7', 'gnm_random_graph(10000,30000),0.9',
+                'gnm_random_graph(10000,30000),1']
+    alpha_cont = [0.7, 0.9, 1]
+    for i, txt in enumerate(txt_cont):
+        d = np.load(path + txt + '.npy')
+        g = nx.gnm_random_graph(10000, 30000)
+        t1 = time.clock()
+        pool = multiprocessing.Pool(processes=6)
+        result = []
+        for p, q in d[:, :2]:
+            result.append(pool.apply_async(func, (p, q, alpha_cont[i], g)))
+
+        pool.close()
+        pool.join()
+
+        data = []
+        for res in result:
+            data.append(res.get())
+
+        print i, txt, 'Time: %.2f s' % (time.clock() - t1)
+        np.save('data\%s-gmm' % txt, data)
