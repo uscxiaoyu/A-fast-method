b68c164fea7b7eb5b5e80eb5d5cfe8cd345592a7
uscxiaoyu <jxndtbxiaoyu@163.com>
2017-12-18 10:02:01 +0800

    海塞矩阵为NAN

--- estimate_bass_2.py
+++ estimate_bass_2.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
-from scipy.optimize import minimize
-from math import e
 import numpy as np
-import time
 import sympy as sy
+import time
+
 
 class Bass_Estimate:
     def __init__(self, s):  # 初始化实例参数
         self.s, self.s_len = np.array(s), len(s)
+        # 求sse对p, q, m的一次和二次导数
         self.p, self.q, self.m = sy.symbols('p q m')
-        sse = self.m * (1 - 1 / (e ** (self.p + self.q))) / (1 + self.q / (self.p * e ** (self.p + self.q))) - self.s[0]
+        sse = self.m * (1 - 1 / (np.exp(self.p + self.q))) / (1 + self.q / (self.p * np.exp(self.p + self.q))) - self.s[0]
         for i in range(1, self.s_len):
-            sse += self.m * (1 - 1 / (e ** ((self.p + self.q) * (i + 1)))) / (1 + self.q / (self.p * e ** ((self.p + self.q) * (i + 1)))) - \
-                   self.m * (1 - 1 / (e ** ((self.p + self.q) * i))) / (1 + self.q / (self.p * e ** ((self.p + self.q) * i))) - self.s[i]
+            sse += self.m * (1 - 1 / (np.exp((self.p + self.q) * (i + 1)))) / (1 + self.q / (self.p * np.exp((self.p + self.q) * (i + 1)))) - \
+                   self.m * (1 - 1 / (np.exp((self.p + self.q) * i))) / (1 + self.q / (self.p * np.exp((self.p + self.q) * i))) - self.s[i]
 
         self.der_p = sy.diff(sse, self.p)
         self.der_q = sy.diff(sse, self.q)
@@ -30,20 +30,25 @@ class Bass_Estimate:
     def f(self, params):  # 如果要使用其它模型，可以重新定义
         p, q, m = params
         t_list = np.arange(1, self.s_len + 1)
-        a = np.array([1 - e ** (- (p + q) * t) for t in t_list])
-        b = np.array([1 + q / p * e ** (- (p + q) * t) for t in t_list])
-        diffu_cont = m * a / b
-        adopt_cont = np.zeros_like(diffu_cont)
-        adopt_cont[0] = diffu_cont[0]
+        a = 1 - np.exp(-(p + q) * t_list)
+        b = 1 + q / p * np.exp(-(p + q) * t_list)
+        diff_cont = m * a / b
+        adopt_cont = np.zeros_like(a)
+        adopt_cont[0] = diff_cont[0]
         for t in range(1, self.s_len):
-            adopt_cont[t] = diffu_cont[t] - diffu_cont[t - 1]
+            adopt_cont[t] = diff_cont[t] - diff_cont[t - 1]
         return adopt_cont
+
+    def sse(self, params):  # 定义适应度函数（mse）
+        a = self.f(params)
+        v = np.sum(np.square(self.s - a))
+        return v / 2  # 均方误
 
     def jacob(self, params):
         p_1 = self.der_p.subs([(self.p, params[0]), (self.q, params[1]), (self.m, params[2])])
         q_1 = self.der_q.subs([(self.p, params[0]), (self.q, params[1]), (self.m, params[2])])
         m_1 = self.der_m.subs([(self.p, params[0]), (self.q, params[1]), (self.m, params[2])])
-        return np.mat([[p_1], [q_1], [m_1]])
+        return np.array([p_1, q_1, m_1], dtype=np.float64)
 
     def hessian(self, params):
         p_2 = self.der_p_2.subs([(self.p, params[0]), (self.q, params[1]), (self.m, params[2])])
@@ -54,14 +59,9 @@ class Bass_Estimate:
         p_m = self.der_p_m.subs([(self.p, params[0]), (self.q, params[1]), (self.m, params[2])])
 
         q_m = self.der_q_m.subs([(self.p, params[0]), (self.q, params[1]), (self.m, params[2])])
-        return np.mat([[p_2, p_q, p_m],
-                          [p_q, q_2, q_m],
-                          [p_m, q_m, m_2]])
-
-    def sse(self, params):  # 定义适应度函数（mse）
-        a = self.f(params)
-        v = np.sum(np.square(self.s - a))
-        return v / 2  # 均方误
+        return np.array([[p_2, p_q, p_m],
+                         [p_q, q_2, q_m],
+                         [p_m, q_m, m_2]], dtype=np.float64)
 
     def r2(self, params):  # 求R2
         f_act = self.f(params)
@@ -77,13 +77,14 @@ class Bass_Estimate:
         while True:
             jac = self.jacob(ini_values)
             hes = self.hessian(ini_values)
-            upd = hes.getI() * jac  # hes.getI()有问题
-            ini_values = ini_values - np.array(upd.getT())
+            inv_hes = np.linalg.inv(hes)
+            upd = np.dot(inv_hes, jac)
+            ini_values = ini_values - upd
             v_sse2 = self.sse(ini_values)
             i += 1
             if abs(v_sse2 - v_sse) <= tol:
                 break
-        return {'sse':v_sse2, 'estimates':ini_values, 'steps':i}  # p,q,m,r2
+        return {'sse': v_sse2, 'estimates': ini_values, 'steps': i}  # p,q,m,r2
 
 
 if __name__=='__main__':
@@ -100,17 +101,10 @@ if __name__=='__main__':
     s = data_set['color televisions'][1]
     t1 = time.clock()
     bas_est = Bass_Estimate(s)
-    res = bas_est.optima_search(np.array([0.001, 0.1, 1.5*sum(s)]), tol=1e-6)
+    res = bas_est.optima_search(np.array([0.001, 0.2, 1.5*sum(s)]), tol=1e-6)
     params = res['estimates']
     x = bas_est.f(params)
     r_2 = bas_est.r2(params)
     print('p:%.4f   q:%.4f   m:%d' % tuple(params))
     print('r^2: %.4f' % r_2)
     print('Time elapsed: %.2fs' % (time.clock() - t1))
-
-
-
-
-
-
-
